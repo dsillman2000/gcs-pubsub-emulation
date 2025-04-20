dbda880cd5a30609adae9534e3815b4f5783f39c
David Sillman <dsillman2000@gmail.com>
2025-04-20 15:36:16 -0400

    feat: setting up filesystem watchdog behavior with kafka

--- environments/local/services/common/config.py
+++ environments/local/services/common/config.py
@@ -1,6 +1,13 @@
+import os
+
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
     KAFKA_BROKER_URL: str = "kafka:9092"
     KAFKA_TOPIC: str = "test-topic"
+    WATCHED_DIRECTORY: str = "./storage"
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        os.makedirs(self.WATCHED_DIRECTORY, exist_ok=True)
--- environments/local/services/producer/src/main.py
+++ environments/local/services/producer/src/main.py
@@ -1,18 +1,39 @@
+import dataclasses
+import datetime
 import json
 import time
 
 from common.config import Settings
 from kafka import KafkaProducer
+from watchdog.events import FileSystemEvent, FileSystemEventHandler
+from watchdog.observers import Observer
+
+settings = Settings()
+
+
+class CreatedFileListener(FileSystemEventHandler):
+    def __init__(self, producer: KafkaProducer, topic: str):
+        self.producer = producer
+        self.topic = topic
+
+    def on_created(self, event: FileSystemEvent):
+        self.producer.send(self.topic, dataclasses.asdict(event))
 
 
 def main():
-    settings = Settings()
-
     producer = KafkaProducer(
         bootstrap_servers=settings.KAFKA_BROKER_URL, value_serializer=lambda x: json.dumps(x).encode("utf-8")
     )
-    for i in range(10):
-        data = {"counter": i}
-        producer.send(settings.KAFKA_TOPIC, value=data)
-        time.sleep(1)
-    producer.close()
+    created_file_listener = CreatedFileListener(producer, settings.KAFKA_TOPIC)
+    observer = Observer()
+    observer.schedule(created_file_listener, path=settings.WATCHED_DIRECTORY, recursive=True)
+    observer.start()
+    try:
+        while True:
+            print("Pinging (time = {})".format(datetime.datetime.now().isoformat()), flush=True)
+            observer.join(1)
+            if not observer.is_alive():
+                break
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
