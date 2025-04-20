8ac5ca5218edc8ae86d1292ed6b0ed0a710da3fb
David Sillman <dsillman2000@gmail.com>
2025-04-20 10:14:43 -0400

    fix: versions.tf for local to only require docker, and vice versa for remote

--- environments/local/versions.tf
+++ environments/local/versions.tf
@@ -1,10 +1,5 @@
 terraform {
   required_providers {
-    # Requires GCP
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 3.5"
-    }
     # Requires Docker
     docker = {
       source  = "kreuzwerker/docker"
--- environments/remote/versions.tf
+++ environments/remote/versions.tf
@@ -5,11 +5,6 @@ terraform {
       source  = "hashicorp/google"
       version = "~> 3.5"
     }
-    # Requires Docker
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 2.0"
-    }
   }
   required_version = ">= 0.12"
 }
